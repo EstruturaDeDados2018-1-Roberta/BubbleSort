0a9ea2669536ee9b6461c7643312111aa70b238f
MateusBMP <mbmp@ic.ufal.br>
2018-09-20 08:49:47 -0300

    Teste escrito com nova função

--- main.c
+++ main.c
@@ -2,6 +2,19 @@
 #include "main.h"
 #include "bubbleSort.h"
 
+void imprimeArray(int array[], int tamanhoMaximo) {
+	int i = 0;
+	for(i = 0; i < tamanhoMaximo - 1; i++) {
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
+
 int main() {
+	int tamanhoMaximo = 10;
+	int array[] = {1, 4, 3, 7, 8, 9, 2, 5, 6, 0};
+	int *pArray = array;
+	bubbleSort(pArray, tamanhoMaximo);
+	imprimeArray(array, tamanhoMaximo);
 	return 0;
 }
