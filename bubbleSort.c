f2b66c546a0a77cb89b7e37df84fcdf0a23e2385
MateusBMP <mbmp@ic.ufal.br>
2018-09-20 08:50:26 -0300

    Correção de bugs

--- bubbleSort.c
+++ bubbleSort.c
@@ -3,18 +3,18 @@
 
 void swap(int *a, int *b) {
 	int aux = *a;
-	a = b;
+	*a = *b;
 	*b = aux;
 }
 
 void comparar(int *valores, int comparadorAtual, int comparadorSeguinte, int tamanhoMaximo) {
-	if(comparadorAtual == tamanhoMaximo) {
+	if(comparadorSeguinte == tamanhoMaximo) {
 		return;
 	} else {
 		if(valores[comparadorAtual] > valores[comparadorSeguinte]) {
 			swap(&valores[comparadorAtual], &valores[comparadorSeguinte]);
 		}
-		comparar(valores, comparadorAtual, comparadorSeguinte++, tamanhoMaximo);
+		comparar(valores, comparadorAtual, comparadorSeguinte + 1, tamanhoMaximo);
 	}
 }
 
@@ -22,11 +22,11 @@ void sortear(int *valores, int comparadorAtual, int tamanhoMaximo) {
 	if(comparadorAtual == tamanhoMaximo) {
 		return;
 	} else {
-		comparar(valores, comparadorAtual, comparadorAtual+1, tamanhoMaximo);
-		sortear(valores, comparadorAtual++, tamanhoMaximo);
+		comparar(valores, comparadorAtual, comparadorAtual + 1, tamanhoMaximo);
+		sortear(valores, comparadorAtual + 1, tamanhoMaximo);
 	}
 }
 
 void bubbleSort(int *valores, int tamanhoMaximo) {
-	sortear(valores, 0, tamanhoMaximo);
+	sortear(valores, 0, tamanhoMaximo - 1);
 }
